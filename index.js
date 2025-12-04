be5da274a842f3cefc6d9e9671c329ee445dd97b
mayurbijarniya <83810766+mayurbijarniya@users.noreply.github.com>
2025-12-03 22:29:41 -0500

    feat: increase request size limits

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ if (process.env.SERVER_ENV !== "development") {
 }
 
 app.use(session(sessionOptions));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
